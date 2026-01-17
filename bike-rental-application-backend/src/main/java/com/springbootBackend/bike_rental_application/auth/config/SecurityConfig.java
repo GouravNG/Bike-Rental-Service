f6f8b47bbb16b05a0d88afbd6d7d0036cd7cf025
Gourav Gunaga <64895467+GouravNG@users.noreply.github.com>
2026-01-17 12:01:42 +0530

    Required CORS setttings for frontendd (#2)

--- bike-rental-application-backend/src/main/java/com/springbootBackend/bike_rental_application/auth/config/SecurityConfig.java
+++ bike-rental-application-backend/src/main/java/com/springbootBackend/bike_rental_application/auth/config/SecurityConfig.java
@@ -11,6 +11,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 import com.springbootBackend.bike_rental_application.auth.jwt.JwtAuthenticationFilter;
 import com.springbootBackend.bike_rental_application.auth.jwt.JwtTokenProvider;
@@ -39,6 +45,7 @@ public class SecurityConfig {
 		JwtAuthenticationFilter jwtFilter = new JwtAuthenticationFilter(jwtTokenProvider);
 		
 		http
+		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
 		.csrf(csrf -> csrf.disable())
 		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 		.authorizeHttpRequests(auth -> auth
@@ -64,5 +71,19 @@ public class SecurityConfig {
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
 		return configuration.getAuthenticationManager();
 	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Auth-Token"));
+		configuration.setExposedHeaders(Arrays.asList("X-Auth-Token"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+
 	
 }
