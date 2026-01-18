ddd23b532694206f731d64f3484f531f69f993cc
kartik naik <kartik naik>
2026-01-18 21:20:45 +0530

    Added Swagger UI and Changes in properties files

---
+++ bike-rental-application-backend/src/main/java/com/springbootBackend/bike_rental_application/auth/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.springbootBackend.bike_rental_application.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Bike Rental Service API")
+                .description("Backend APIs for Bike Rental Application")
+                .version("1.0.0"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
